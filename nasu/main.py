7e4205cb05f7175fc48c0c383b78f794178d4f58
dannyso16 <osatoshi.11@gmail.com>
2019-08-15 01:02:18 +0900

    Add animation of Hiyoko, Uribo, and clouds
    アニメーションの追加
    うりぼーが進行方向を向くようになった

--- nasu/main.py
+++ nasu/main.py
@@ -28,7 +28,7 @@ def collide(ax:int, ay:int, bx:int, by:int) -> bool:
 # ===== draw assets =====
 def drawRingo(x, y):
     pyxel.blt(x, y, 0,
-              0, 0, 16, 16, pyxel.COLOR_LIGHTGRAY)
+                0, 0, 16, 16, pyxel.COLOR_LIGHTGRAY)
 
 def drawOrange(x, y):
     pyxel.blt(x, y, 0,
@@ -43,15 +43,36 @@ def drawBucket(x, y):
                 0, 16, 16, 16, pyxel.COLOR_LIGHTGRAY)
 
 def drawHiyoko(x, y):
+    anim_frame_count = pyxel.frame_count // 3
+    idx = anim_frame_count % 4 # animation has 4 images
     pyxel.blt(x, y, 0,
-                16, 16, 16, 16, pyxel.COLOR_LIGHTGRAY)
+                idx*16, 32, 16, 16, pyxel.COLOR_LIGHTGRAY)
 
 def drawUribo(x, y):
+    anim_frame_count = pyxel.frame_count // 3
+    idx = anim_frame_count % 4 # animation has 4 images
+    sign = -1 if isLookingLeft else 1
     pyxel.blt(x, y, 0,
-                32, 16, 16, 16, pyxel.COLOR_LIGHTGRAY)
+                idx*16, 48, sign*16, 16, pyxel.COLOR_LIGHTGRAY)
 
-def drawBackGround():
-    pyxel.blt(0, 0, 1, 0, 0, 250, 150)
+def drawBackGround(earthQuake=False):
+    CLOUD_OFFSET = 56 # the maximum width of clouds
+    anim_frame_count = pyxel.frame_count // 3
+
+    # Three clouds move left to right side
+    # when clouds come to the right edge, transfer it to the left edge
+    mod = (pyxel.width + CLOUD_OFFSET)
+    cloud1X = anim_frame_count       % mod - CLOUD_OFFSET
+    cloud2X = (anim_frame_count-100) % mod - CLOUD_OFFSET
+    cloud3X = (anim_frame_count-180) % mod - CLOUD_OFFSET
+
+    pyxel.cls(pyxel.COLOR_CYAN)
+    pyxel.blt(cloud1X,  8,  1, 16,  8, 56, 32)
+    pyxel.blt(cloud2X,  16, 1, 192, 8, 48, 32)
+    pyxel.blt(cloud3X,  10, 1, 16,  8, 56, 32)
+
+    amplitude = anim_frame_count%2 if earthQuake else 0
+    pyxel.blt(0, 48+amplitude, 1, 0, 48, 250, 150) # ground and trees
 
 def drawScore(score:int):
     pyxel.text(10, 142, 'SCORE: {}'.format(score), pyxel.COLOR_WHITE)
@@ -81,13 +102,16 @@ hiyokoY = HORIZONTAL_Y
 ringoX = randrange(0, pyxel.width)
 ringoY = 0
 score = 0
+isLookingLeft = True
 
 while True:
     # keyboard input
     if pyxel.btn(pyxel.KEY_RIGHT):
         hiyokoX += 4
+        isLookingLeft = True
     if pyxel.btn(pyxel.KEY_LEFT):
         hiyokoX -= 4
+        isLookingLeft = False
 
     # 当たり判定
     if collide(hiyokoX, hiyokoY, ringoX, ringoY):
@@ -102,7 +126,7 @@ while True:
 
     # drawAll()
     drawBackGround()
-    drawRottenRingo(ringoX, ringoY)
+    drawRingo(ringoX, ringoY)
     drawHiyoko(hiyokoX, hiyokoY)
     drawScore(score)
     pyxel.flip()
